f62e8808b54f56205670156ceafb454d191b65a3
OHHHHHHei <2859611604@qq.com>
2026-04-29 18:52:32 +0800

    Revert longer top-k to 64

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ python3 -u "${SCRIPT_DIR}/train.py" \
     --emb_skip_threshold 1000000 \
     --use_ns_output_fusion \
     --seq_encoder_type longer \
-    --seq_top_k 128 \
+    --seq_top_k 64 \
     --seq_max_lens seq_a:256,seq_b:256,seq_c:1024,seq_d:1024 \
     --split_mode time \
     --split_time_col timestamp \
